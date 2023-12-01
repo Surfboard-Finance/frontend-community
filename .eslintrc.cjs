3d3131fd1b0871cc00ea955a6a52a5f355f5447b
alkia <maxime@alkia.net>
2023-12-01 14:38:07 +0700

    Eslit correction

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,10 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
+/* require('@rushstack/eslint-patch/modern-module-resolution')
+*/
 module.exports = {
   root: true,
   'extends': [
+    'eslint:recommended',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
